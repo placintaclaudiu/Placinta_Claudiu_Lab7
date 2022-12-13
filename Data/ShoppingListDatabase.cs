76aa2af54f6f44f4feae6a1e7fdbf8d6e8dbb2ed
Placinta Claudiu <claudiuplacinta33@gmail.com>
2022-12-13 18:45:58 +0200

    sarcina lab9

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -17,6 +17,8 @@ namespace Placinta_Claudiu_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
+
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -83,6 +85,31 @@ namespace Placinta_Claudiu_Lab7.Data
             + " inner join ListProduct LP"
             + " on P.ID = LP.ProductID where LP.ShopListID = ?",
             shoplistid);
+        }
+
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
+
+        public Task<int> DeleteListProductAsync(ListProduct listp)
+        {
+            return _database.DeleteAsync(listp);
+        }
+        public Task<List<ListProduct>> GetListProducts()
+        {
+            return _database.QueryAsync<ListProduct>("select * from ListProduct");
         }
     }
 }
